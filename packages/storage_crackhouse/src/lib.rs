11fc2aa5bd70f7e0640759bb2c623b8ffecf932e
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2026-05-15 18:24:18 +0300

    crack

---
+++ packages/storage_crackhouse/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn init() {
+    tracing::info!("Hello, world!");
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub extern crate consensus_crackhead;
+pub extern crate net_crackpipe;
+pub extern crate storage_crackhouse;
