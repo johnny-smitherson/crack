a8903edb469e03dd3e0089b71f487d9fc579ff75
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2026-06-28 20:33:30 +0300

    skybox

--- crack_demo/demo_resolution_selector_web_bevy/src/plugins/main_scene_plugin.rs
+++ crack_demo/demo_resolution_selector_web_bevy/src/plugins/main_scene_plugin.rs
@@ -1,4 +1,6 @@
 use bevy::core_pipeline::tonemapping::Tonemapping;
+use bevy::core_pipeline::Skybox;
+use bevy::render::render_resource::{TextureViewDescriptor, TextureViewDimension};
 use bevy::prelude::*;
 
 pub struct MainScenePlugin;
@@ -13,12 +15,27 @@ impl Plugin for MainScenePlugin {
                 crate::ui_egui::web_set_loading_status(false, "");
             }),
         );
+        app.add_systems(Update, convert_and_apply_skybox);
         info!("done loading: MainScenePlugin");
     }
 }
 
-fn setup_camera_and_load(mut commands: Commands) {
-    // Keep only default camera spawning
+#[derive(Resource)]
+pub struct SkyboxState {
+    pub handle: Handle<Image>,
+    pub loaded: bool,
+}
+
+fn setup_camera_and_load(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let skybox_texture_url = format!("{}skybox_clouds.png", crate::config::DATA_BASE_URL);
+    let skybox_handle = asset_server.load(skybox_texture_url);
+    
+    commands.insert_resource(SkyboxState {
+        handle: skybox_handle,
+        loaded: false,
+    });
+
+    // Keep only default camera spawning with Skybox component
     commands.spawn((
         Transform::from_xyz(0.0, 10.5, -30.0).looking_at(Vec3::ZERO, Vec3::Y),
         Camera {
@@ -27,14 +44,70 @@ fn setup_camera_and_load(mut commands: Commands) {
         },
         Camera3d::default(),
         Tonemapping::None,
-    ));
-
-    // Spawn directional light (sun)
-    commands.spawn((
-        DirectionalLight {
-            illuminance: 10000.0,
+        Skybox {
+            image: None,
+            brightness: 1000.0,
             ..default()
         },
-        Transform::from_xyz(10.0, 20.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        AmbientLight {
+            color: Color::srgb(0.75, 0.85, 1.0),
+            brightness: 1000.0,
+            ..default()
+        },
     ));
+}
+
+fn convert_and_apply_skybox(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut images: ResMut<Assets<Image>>,
+    state: Option<ResMut<SkyboxState>>,
+    mut q_camera: Query<(Entity, &mut Skybox), With<Camera3d>>,
+) {
+    let Some(mut state) = state else {
+        return;
+    };
+    
+    if state.loaded {
+        return;
+    }
+    
+    if let Some(load_state) = asset_server.get_load_state(&state.handle) {
+        if matches!(load_state, bevy::asset::LoadState::Loaded) {
+            if let Some(mut image) = images.get_mut(&state.handle) {
+                let layers = image.height() / image.width();
+                if layers == 6 {
+                    if let Err(err) = image.reinterpret_stacked_2d_as_array(layers) {
+                        error!("Failed to reinterpret skybox image: {:?}", err);
+                    } else {
+                        image.texture_view_descriptor = Some(TextureViewDescriptor {
+                            dimension: Some(TextureViewDimension::Cube),
+                            ..default()
+                        });
+                        
+                        info!("Skybox cubemap configured successfully! Applying to camera...");
+                        for (entity, mut skybox) in &mut q_camera {
+                            skybox.image = Some(state.handle.clone());
+                            
+                            // Insert EnvironmentMapLight so that the skybox influences the lighting
+                            commands.entity(entity).insert(EnvironmentMapLight {
+                                diffuse_map: state.handle.clone(),
+                                specular_map: state.handle.clone(),
+                                intensity: 2000.0,
+                                ..default()
+                            });
+                        }
+                        state.loaded = true;
+                    }
+                } else {
+                    error!(
+                        "Skybox image has invalid aspect ratio for cubemap (layers: {}, width: {}, height: {})",
+                        layers,
+                        image.width(),
+                        image.height()
+                    );
+                }
+            }
+        }
+    }
 }
--- start_web2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-# ./build_worker.sh
-
-export RUST_LOG=info
-
-dx serve --keep-names  --package web_frontend "$@"
