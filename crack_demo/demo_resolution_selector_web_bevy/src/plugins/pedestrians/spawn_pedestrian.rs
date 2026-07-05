e8da9d738ecb1d177fa97c8afb2233c1878a5fb6
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2026-07-05 15:12:41 +0300

    character controller

--- crack_demo/demo_resolution_selector_web_bevy/src/plugins/pedestrians/spawn_pedestrian.rs
+++ crack_demo/demo_resolution_selector_web_bevy/src/plugins/pedestrians/spawn_pedestrian.rs
@@ -1,6 +1,7 @@
 //! The pedestrian spawn path: turn a [`SpawnPedestrianEvent`] into a live, aligned,
 //! classified pedestrian entity.
 
+use avian3d::collision::collider::{ColliderConstructor, ColliderConstructorHierarchy};
 use avian3d::prelude::Collider;
 use bevy::ecs::relationship::Relationship;
 use bevy::prelude::*;
@@ -73,6 +74,8 @@ pub fn spawn_pedestrian_observer(
         .with_children(|parent| {
             parent.spawn((
                 WorldAssetRoot(handle),
+                // Do not enable physics on mesh - it would need to be reinterpolated!
+                //  ColliderConstructorHierarchy::new(ColliderConstructor::TrimeshFromMesh),
                 Transform::IDENTITY,
                 Visibility::default(),
                 InheritedVisibility::default(),
@@ -145,9 +148,9 @@ pub fn init_pedestrians_system(
             };
 
             if let Some(mesh) = meshes.get(mesh_handle) {
-                if let Some(collider) = Collider::trimesh_from_mesh(mesh) {
-                    commands.entity(*ent).insert(collider);
-                }
+                // if let Some(collider) = Collider::trimesh_from_mesh(mesh) {
+                //     commands.entity(*ent).insert(collider);
+                // }
                 if let Some(bevy::render::mesh::VertexAttributeValues::Float32x3(positions)) =
                     mesh.attribute(Mesh::ATTRIBUTE_POSITION)
                 {
