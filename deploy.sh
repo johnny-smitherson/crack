dc6bcb3e6d326d80086b99e58b89a502db6612b4
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2026-07-08 14:03:06 +0300

    fix deploy pls

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -ex
-./build_worker.sh
 
 (
     cd crack_demo/demo_resolution_selector_web_bevy
+    trunk clean
     trunk build --release true
 )
+
+./build_worker.sh
 
 rsync -av --exclude 'target' --exclude '.git' --exclude ".venv" --exclude _data/3d_data_v2/data_cache/ --exclude '*.bytes' ./ dj-vaslui:crack/
 
--- start_game_web.sh
+++ start_game_web.sh
@@ -2,6 +2,7 @@
 set -ex
 
 export RUST_LOG=info
+./build_worker.sh
 
 cd crack_demo/demo_resolution_selector_web_bevy
 trunk watch "$@"
